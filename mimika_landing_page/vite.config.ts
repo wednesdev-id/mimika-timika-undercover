c0d2e1a6fee0973b114ca972042fe1a411c2488a
Akichan0201 <adzkiaalmaghania701@gmail.com>
2026-01-20 16:20:51 +0700

    chore: force redeploy mimika

--- mimika_landing_page/vite.config.ts
+++ mimika_landing_page/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
+// Force Redeploy Check
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
